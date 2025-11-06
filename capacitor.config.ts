269f751cb9d2d6e7e81502198b821ca3ef298050
WCEdmonds <wcedmonds28@gmail.com>
2025-11-06 12:35:22 -0500

    static

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'fit.mysquad.app',
   appName: 'FitSquad',
   webDir: 'out',
-  server: {
-    url: 'mysquad.fit',
-    cleartext: true
-  }
 };
 
 export default config;
